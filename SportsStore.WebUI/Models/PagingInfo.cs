f4293dc4fdde3fab99c76a98532b348d7dd4851f
Rafal <rafal_czosnyka@tlen.pl>
2019-11-23 08:14:18 +0100

    Add pading

---
+++ SportsStore.WebUI/Models/PagingInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace SportsStore.WebUI.Models
+{
+    //klasa ulatwiajaca przekaz danych miedzy kontrolerem a widokiem
+    //nie wchodzi w sklad modelu domeny
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        public int TotalPages
+        {
+            get { return (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage); }
+        }
+    }
+}
